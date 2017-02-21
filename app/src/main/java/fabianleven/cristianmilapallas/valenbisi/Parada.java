68cd9b6c321b7d0e33d774f6981181e142acc69b
PhysicsFabi <uagwi@student.kit.edu>
2017-02-21 15:26:24 +0100

    adds the json file adds the parada class

--- app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parada.java
+++ app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parada.java
@@ -5,4 +5,25 @@ package fabianleven.cristianmilapallas.valenbisi;
  */
 
 public class Parada {
+    private String name;
+    private int number;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Parada(String name, int number, String address) {
+        this.name = name;
+        this.number = number;
+        this.address = address;
+    }
 }
