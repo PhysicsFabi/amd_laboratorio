936e9728b400b828b99085af1e20f1ec8654e5bc
PhysicsFabi <uagwi@student.kit.edu>
2017-03-07 15:37:35 +0100

    sets the title of the detail activity

--- app/src/main/java/fabianleven/cristianmilapallas/valenbisi/DetalleParada.java
+++ app/src/main/java/fabianleven/cristianmilapallas/valenbisi/DetalleParada.java
@@ -10,5 +10,6 @@ public class DetalleParada extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_parada);
         Parada parada = (Parada) getIntent().getSerializableExtra(ListaParadas.STATION_KEY);
+        setTitle(parada.name);
     }
 }
