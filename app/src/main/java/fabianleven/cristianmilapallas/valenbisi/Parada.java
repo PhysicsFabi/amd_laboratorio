099b2c6824c51d259f871b7e4f0afcb551335c8a
PhysicsFabi <uagwi@student.kit.edu>
2017-03-07 15:26:04 +0100

    Pass parada from one activity to an other

--- app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parada.java
+++ app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parada.java
@@ -1,10 +1,13 @@
 package fabianleven.cristianmilapallas.valenbisi;
 
+import java.io.Serializable;
+
 /**
  * Created by Fabi on 21.02.2017.
  */
 
-public class Parada {
+public class Parada implements Serializable {
+    private static final long serialVersionUID = -2004949158290397639L;
     public String name;
     public int number;
     public String address;
