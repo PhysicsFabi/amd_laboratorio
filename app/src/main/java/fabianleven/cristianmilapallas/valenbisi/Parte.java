9ac079a7f45ec68fdb9f1a560293a68c7049c36d
Fabi <Fabi@TERMINATOR>
2017-03-21 16:31:45 +0100

    adds the database logic

--- app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parte.java
+++ app/src/main/java/fabianleven/cristianmilapallas/valenbisi/Parte.java
@@ -5,31 +5,57 @@ package fabianleven.cristianmilapallas.valenbisi;
  */
 
 public class Parte {
-    public enum Status {
-        OPEN,
-        IN_PROGRESS,
-        CLOSED
+    public enum STATUS {
+        OPEN(0),
+        IN_PROGRESS(1),
+        CLOSED(2);
+        private int val;
+
+        STATUS(int val) {
+            this.val = val;
+        }
+
+        public int getVal() {
+            return this.val;
+        }
     }
 
-    public enum Type {
-        MECHANICAL,
-        ELECTRICAL,
-        PAINTING,
-        CONSTRUCTION
+    public enum TYPE {
+        MECHANICAL(0),
+        ELECTRICAL(1),
+        PAINTING(2),
+        CONSTRUCTION(3);
+
+        private int val;
+
+        TYPE(int val) {
+            this.val = val;
+        }
+
+        public int getVal() {
+            return this.val;
+        }
+
     }
 
+    private String id;
     private String name;
     private String description;
     private int stationId;
-    private Status status;
-    private Type type;
+    private STATUS status;
+    private TYPE type;
 
-    public Parte(String name, String description, int stationId, Status status, Type type) {
+    public Parte(String id, String name, String description, int stationId, STATUS status, TYPE type) {
+        this.id = id;
         this.name = name;
         this.description = description;
         this.stationId = stationId;
         this.status = status;
         this.type = type;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
@@ -44,11 +70,11 @@ public class Parte {
         return stationId;
     }
 
-    public Status getStatus() {
+    public STATUS getStatus() {
         return status;
     }
 
-    public Type getType() {
+    public TYPE getType() {
         return type;
     }
 }
