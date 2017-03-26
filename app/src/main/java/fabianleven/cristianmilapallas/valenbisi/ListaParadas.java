1068ce79a4c5e417ae2c4bcf080e35a9844e3fbf
Fabi <Fabi@TERMINATOR>
2017-03-27 00:53:58 +0200

    refreshes ListaParadas when the data changes

--- app/src/main/java/fabianleven/cristianmilapallas/valenbisi/ListaParadas.java
+++ app/src/main/java/fabianleven/cristianmilapallas/valenbisi/ListaParadas.java
@@ -11,23 +11,30 @@ public class ListaParadas extends AppCompatActivity {
 
     public static final String logTag = "Valenbisi";
     public static final String STATION_KEY = "station";
+    private AdapterParada adapterParada;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_paradas);
         ListView listView = (ListView) findViewById(R.id.station_list);
-        final AdapterParada adapterParada = new AdapterParada(getApplicationContext());
+        adapterParada = new AdapterParada(getApplicationContext());
         listView.setAdapter(adapterParada);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent in = new Intent(getApplicationContext(), DetalleParada.class);
-            Parada parada = (Parada) adapterParada.getItem(position);
-            // parada is serializable
-            in.putExtra(STATION_KEY, parada);
-            startActivity(in);
-        }
-    });
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent in = new Intent(getApplicationContext(), DetalleParada.class);
+                Parada parada = (Parada) adapterParada.getItem(position);
+                // parada is serializable
+                in.putExtra(STATION_KEY, parada);
+                startActivity(in);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        adapterParada.notifyDataSetChanged();
     }
 }
